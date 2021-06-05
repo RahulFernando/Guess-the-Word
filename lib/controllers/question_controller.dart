34115cc621d93c38fec3badbb727adc632e7afad
plhewa <plhewa2@gmail.com>
2021-06-05 18:15:42 +0530

    Changes to the question model

--- lib/controllers/question_controller.dart
+++ lib/controllers/question_controller.dart
@@ -7,13 +7,13 @@ class QuestionController {
   // get all quesions
   Stream<List<Question>> getQuesions() {
     return _db.collection('questions').snapshots().map((snapshot) =>
-        snapshot.docs.map((doc) => Question.fromJson(doc.data())).toList());
+        snapshot.docs.map((doc) => Question.fromJson(doc.data(),doc.reference)).toList());
   }
 
   // get question by id
   Future<Question> getQuestion(String id) {
     return _db.collection('questions').document(id).get().then((value) {
-      return Question.fromJson(value.data());
+      return Question.fromJson(value.data(),value.reference);
     });
   }
 }
--- lib/models/question.dart
+++ lib/models/question.dart
@@ -1,12 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Question {
+
   final String question;
   final List<bool> answers;
   final List<String> options;
 
-  Question({this.question, this.answers, this.options});
+  DocumentReference id;
 
-  factory Question.fromJson(Map<String, dynamic> json) {
+  Question({this.id, this.question, this.answers, this.options});
+  
+  factory Question.fromJson(Map<String, dynamic> json , DocumentReference docRef) {
     return Question(
+      id: docRef,
       question: json['question'] as String,
       answers: List<bool>.from(json['answers'].map((y) => y)),
       options: List<String>.from(json['options'].map((x) => x)),
@@ -15,6 +21,7 @@ class Question {
 
   Map<String, dynamic> toMap() {
     return {
+      "id":id,
       "question": question,
       "answers": answers,
       "options": options
