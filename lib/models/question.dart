9143b11276621a36dd3598ecc11d3151dd819b57
RahulFernando <rahulrulz680@gmail.com>
2021-06-05 17:48:33 +0530

    modal change

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -1,17 +1,13 @@
 class Question {
-  final int id;
   final String question;
-  final int answer;
   final List<bool> answers;
   final List<String> options;
 
-  Question({this.id, this.question, this.answer, this.answers, this.options});
+  Question({this.question, this.answers, this.options});
 
   factory Question.fromJson(Map<String, dynamic> json) {
     return Question(
-      id: json['id'] as int,
       question: json['question'] as String,
-      answer: json['answer'] as int,
       answers: List<bool>.from(json['answers'].map((y) => y)),
       options: List<String>.from(json['options'].map((x) => x)),
     );
@@ -19,9 +15,7 @@ class Question {
 
   Map<String, dynamic> toMap() {
     return {
-      "id": id,
       "question": question,
-      "answer": answer,
       "answers": answers,
       "options": options
     };
