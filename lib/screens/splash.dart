84cc3e074cfeab6665c34db4732df79d1b62d97d
DananjayaPerera <dananjayaperera655@gmail.com>
2021-06-07 11:46:14 +0530

    minor changes

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,10 +1,7 @@
-import 'dart:async';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:guess_app/screens/welcome.dart';
-import 'package:guess_app/utils/color.dart';
 import 'package:page_transition/page_transition.dart';
-
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -12,13 +9,11 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body:AnimatedSplashScreen(
+        resizeToAvoidBottomInset: false,
+        body: AnimatedSplashScreen(
           duration: 4000,
           splash: 'assets/img/Logo.png',
           splashIconSize: 130,
